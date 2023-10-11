1e2202f4c99bff9161e964ea83d338c944a48889
TheSikandar <136466801+TheSikandar@users.noreply.github.com>
2023-10-11 21:48:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/authRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path { path } from "path"; 
+import path from "path"; 
 //configure env
 dotenv.config();
 
