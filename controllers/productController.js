31852125a0fef117266f4494292eaa869343ac73
TheSikandar <136466801+TheSikandar@users.noreply.github.com>
2023-12-14 14:59:01 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -62,7 +62,7 @@ export const getProductController = async (req,res) =>{
         .find({})
         .populate("category")
         .select("-photo")
-        .limit(12)
+        .limit(120)
         .sort({createdAt:-1});
         res.status(200).send({
             success:true,
